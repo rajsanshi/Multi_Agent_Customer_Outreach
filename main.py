fc026f2d4c6aa36a97575d7c038eb2e44f2f9394
s3975993 <s3975993@student.rmit.edu.au>
2024-12-22 10:59:48 +1100

    created tasks

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import warnings
 from crewai import Agent, Task, Crew,LLM
 import os
 from dotenv import load_dotenv
+from Agents import sales_rep_agent,lead_sales_rep_agent
+from Tasks import lead_profiling_task,personalized_outreach_task
 
 load_dotenv
 
@@ -13,4 +15,26 @@ llm = LLM(
     model="groq/llama-3.2-90b-text-preview",
     temperature=0.7
 )
+
+crew = Crew(
+    agents=[sales_rep_agent, 
+            lead_sales_rep_agent],
+    
+    tasks=[lead_profiling_task, 
+           personalized_outreach_task],
+	
+    verbose=2,
+	memory=True
+)
+
+inputs = {
+    "lead_name": "OpenLearningHub",
+    "industry": "Online Education",
+    "key_decision_maker": "Sundar Pichai",
+    "position": "Chief Innovation Officer",
+    "milestone": "new AI-driven course platform launch"
+}
+
+result = crew.kickoff(inputs=inputs)
+
 
